e647ac4f7fb8d11fa3f439772110b08f63a1cb6c
selelel <136282944+selelel@users.noreply.github.com>
2023-11-23 14:45:23 +0800

    add content to footer

--- src/Components/Main/Footer.tsx
+++ src/Components/Main/Footer.tsx
@@ -1,5 +1,24 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="mx-10 flex flex-col items-center text-sm">
+      <h1 className="font-bold text-lg">{"<sel/>"}</h1>
+
+      <ul className="flex gap-3 cursor-pointer">
+        <li>
+          <a href="./#hero">Home</a>
+        </li>
+        <li>
+          <a href="/#about">About</a>
+        </li>
+        <li>
+          <a href="/#project">Project</a>
+        </li>
+      </ul>
+
+      <h1 className="text-xs">Developed by Jan Russel "Sel" Gorembalem</h1>
+      <h1 className="text-xs">@2023</h1>
+    </div>
+  );
 }
 
 export default Footer;
