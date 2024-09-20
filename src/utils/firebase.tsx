d53e823e9146f060489d0044235b5afe5a866379
Jan Russel Gorembalem <janrusselgorembalem4@gmail.com>
2024-09-20 16:05:06 +0800

    Fix Firebase configuration

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
