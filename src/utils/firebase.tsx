9200e8c380ca12e5bea4a6b24cebcd003bd1f112
Jan Russel Gorembalem <janrusselgorembalem4@gmail.com>
2024-10-30 20:44:18 +0800

    Deploy

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -11,5 +11,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(firebaseConfig)
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
