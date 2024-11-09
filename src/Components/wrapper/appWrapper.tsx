f1052b5c94b2b1e1535429a4308cd263f50cf72c
Jan Russel Gorembalem <janrusselgorembalem4@gmail.com>
2024-11-09 16:02:20 +0800

    [FEAT] Metadata fixibng

--- src/Components/wrapper/appWrapper.tsx
+++ src/Components/wrapper/appWrapper.tsx
@@ -3,13 +3,9 @@ import appConfig from '../../config/appConfig';
 
 const AppWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     useEffect(() => {
-        console.log(appConfig.title)
         document.title = appConfig.title!;
 
-        const metaDescription = document.querySelector(
-            'meta[name="description"]'
-        );
-
+        const metaDescription = document.querySelector('meta[name="description"]');
         if (metaDescription) {
             metaDescription.setAttribute('content', appConfig.description!);
         } else {
@@ -18,6 +14,23 @@ const AppWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             newMetaDescription.content = appConfig.description!;
             document.head.appendChild(newMetaDescription);
         }
+
+        const setOrCreateMetaTag = (property: string, content: string) => {
+            let metaTag = document.querySelector(`meta[property="${property}"]`);
+            if (metaTag) {
+                metaTag.setAttribute('content', content);
+            } else {
+                metaTag = document.createElement('meta');
+                metaTag.setAttribute('property', property);
+                metaTag.setAttribute('content', content);
+                document.head.appendChild(metaTag);
+            }
+        };
+
+        setOrCreateMetaTag('og:title', appConfig.title!);
+        setOrCreateMetaTag('og:description', appConfig.description!);
+        setOrCreateMetaTag('og:image', '%PUBLIC_URL%/logo192.png');
+        setOrCreateMetaTag('og:url', window.location.href);
     }, []);
 
     return <>{children}</>;
