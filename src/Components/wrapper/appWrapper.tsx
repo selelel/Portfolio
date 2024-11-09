31a0a7d91e6cc0dd7f08badc055e5ca087189c00
Jan Russel Gorembalem <136282944+selelel@users.noreply.github.com>
2024-11-09 15:50:16 +0800

    Update appWrapper.tsx

--- src/Components/wrapper/appWrapper.tsx
+++ src/Components/wrapper/appWrapper.tsx
@@ -2,24 +2,21 @@ import React, { useEffect } from 'react';
 import appConfig from '../../config/appConfig';
 
 const AppWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    useEffect(() => {
-        console.log(appConfig.title)
-        document.title = appConfig.title!;
+    document.title = appConfig.title || "Jan Russel Gorembalem";
 
-        const metaDescription = document.querySelector(
-            'meta[name="description"]'
-        );
+    const metaDescription = document.querySelector(
+        'meta[name="description"]'
+    );
 
-        if (metaDescription) {
-            metaDescription.setAttribute('content', appConfig.description!);
-        } else {
-            const newMetaDescription = document.createElement('meta');
-            newMetaDescription.name = 'description';
-            newMetaDescription.content = appConfig.description!;
-            document.head.appendChild(newMetaDescription);
-        }
-    }, []);
-
+    if (metaDescription) {
+        metaDescription.setAttribute('content', appConfig.description!);
+    } else {
+        const newMetaDescription = document.createElement('meta');
+        newMetaDescription.name = 'description';
+        newMetaDescription.content = appConfig.description!;
+        document.head.appendChild(newMetaDescription);
+    }
+    
     return <>{children}</>;
 };
 
